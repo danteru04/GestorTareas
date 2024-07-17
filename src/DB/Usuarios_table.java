78beec5eb29aaaf98601e32a6acdee00fbf9963a
SoporteSJR <becarioit@farmapiel.com>
2024-07-17 13:29:51 -0600

    Se creo conexion a base de datos tabla usuarios se creo

---
+++ src/DB/Usuarios_table.java
@@ -0,0 +1,45 @@
+package DB;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Usuarios_table {
+    String database;
+    String user;
+    String password;
+    Connection connection;
+
+    public Usuarios_table(){
+        this.database  = "gestortareas";
+        this.user  = "dante";
+        this.password  = "2733920";
+        try{
+            connection = DriverManager.getConnection(
+                    "jdbc:mariadb://localhost:3306/" + this.database +
+                            "?user=" + this.user +
+                            "&password=" + this.password
+            );
+            System.out.println("Conexión establecida con éxito");
+            Statement statement = connection.createStatement();
+            int rowsaffected = statement.executeUpdate("CREATE TABLE IF NOT EXISTS usuarios (" +
+                    "id_usuario INT PRIMARY KEY AUTO_INCREMENT," +
+                    "nombre VARCHAR (255) ," +
+                    "ap_paterno VARCHAR (50)," +
+                    "ap_materno VARCHAR (50)," +
+                    "email VARCHAR (255)," +
+                    "password VARCHAR (255)) ENGINE=INNODB;");
+
+            System.out.println(rowsaffected + " filas han sido cambiadas");
+        }
+        catch (SQLException e){
+            System.out.println("La conexión de la Base de Datos "+ this.database + " ha fallado");
+            System.out.println(e);
+            System.exit(0);
+        }
+    }
+
+
+
+}
