4ea2751288a2ad53ba61591b8999df51b4bd367e
SoporteSJR <becarioit@farmapiel.com>
2024-07-17 14:00:35 -0600

    Introduccion a GUI

---
+++ src/Vistas/loginView.java
@@ -0,0 +1,15 @@
+package Vistas;
+
+import javax.swing.*;
+
+public class loginView {
+    private JPanel rootPanel;
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("loginView");
+        frame.setContentPane(new loginView().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+}
