bcbd963aad3c65597038b2009b6a3788ff8a37c1
Dante Reyes <126126124+danteru04@users.noreply.github.com>
2024-07-18 09:37:28 -0600

    Update Main.java
    test de commit, comentario en codigo

--- src/Main.java
+++ src/Main.java
@@ -5,6 +5,7 @@ class Main{
     public static void main(String[] args) {
         System.out.println("Hello World!");
 
+        //Realizar conexion a base de datos y crea si no existe la tabla usuarios
         Usuarios_table conexion = new Usuarios_table();
     }
 }
