b2ef0c113776cbf33c6b081400453521ca05c8e4
SoporteSJR <becarioit@farmapiel.com>
2024-07-19 13:58:57 -0600

    Introduccion a GUI Size Label and Button

--- src/Vistas/loginView.java
+++ src/Vistas/loginView.java
@@ -1,16 +1,29 @@
 package Vistas;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class loginView {
     private JPanel rootPanel;
+    private JButton helloButton;
+    private JLabel helloLabel;
 
     public static void main(String[] args) {
         JFrame frame = new JFrame("loginView");
         frame.setContentPane(new loginView().rootPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
-        frame.setSize(600, 600);
         frame.setVisible(true);
+    }
+
+
+    public loginView() {
+        helloButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                helloLabel.setText("Hello World!");
+            }
+        });
     }
 }
