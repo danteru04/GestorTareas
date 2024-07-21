1bdcdfd252029346178697c77ede4e36da46eb76
dante <dante.isaac.ru@hotmail.com>
2024-07-21 17:00:16 -0600

    Create user method, gui form for login

--- src/Main.java
+++ src/Main.java
@@ -3,8 +3,7 @@ import DB.Usuarios_table;
 
 class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
-
         Usuarios_table conexion = new Usuarios_table();
+
     }
 }
