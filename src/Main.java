b141472c5278497f3489c3ce8e3c88eb3b89f914
SoporteSJR <becarioit@farmapiel.com>
2024-07-24 13:32:03 -0600

    Getters Usuarios_table login Pruebas

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,13 @@
 import DB.Usuarios_table;
 
+import javax.swing.*;
+
 
 class Main{
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        UIManager.LookAndFeelInfo info[] = UIManager.getInstalledLookAndFeels();
+        for(UIManager.LookAndFeelInfo look: info)
+            System.out.println(look.getClassName());
 
         Usuarios_table conexion = new Usuarios_table();
     }
--- src/Vistas/loginView.java
+++ src/Vistas/loginView.java
@@ -1,29 +1,57 @@
 package Vistas;
 
 import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
 
-public class loginView {
+public class loginView //extends JPanel
+    {
     private JPanel rootPanel;
-    private JButton helloButton;
-    private JLabel helloLabel;
+    private JLabel tituloLabel;
+    private JTextField usuarioTextfield;
+    private JButton entrarButton;
+    private JLabel usuarioLabel;
+    private JLabel passwordLabel;
+    private JTextField passwordTextfield;
+    private JLabel mensajeLabel;
 
-    public static void main(String[] args) {
+//    @Override
+//    protected void paintComponent(Graphics g) {
+//        super.paintComponent(g);
+//        Graphics2D g2d = (Graphics2D) g;
+//        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
+//        int w = getWidth(), h = getHeight();
+//        Color color1 = Color.RED;
+//        Color color2 = Color.GREEN;
+//        GradientPaint gp = new GradientPaint(0, 0, color1, w, h, color2);
+//        g2d.setPaint(gp);
+//        g2d.fillRect(0, 0, w, h);
+//    }
+
+    public static void main(String[] args) throws Exception{
+        UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
         JFrame frame = new JFrame("loginView");
         frame.setContentPane(new loginView().rootPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.pack();
+        frame.setSize(400,400);
+        frame.setUndecorated(true);
+        frame.setOpacity(0);
         frame.setVisible(true);
     }
 
+//    public static void main(String[] args) {
+//        SwingUtilities.invokeLater(new Runnable() {
+//            @Override
+//            public void run() {
+//                JFrame frame = new JFrame();
+//                loginView panel = new loginView();
+//                frame.add(panel);
+//                frame.setSize(200, 200);
+//                frame.setLocationRelativeTo(null);
+//                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//                frame.setVisible(true);
+//            }
+//        });
+//    }
 
-    public loginView() {
-        helloButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                helloLabel.setText("Hello World!");
-            }
-        });
-    }
 }
