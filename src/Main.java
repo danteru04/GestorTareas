9be958fbd2bf3c68469c4f6e538cba0cdf054fd0
SoporteSJR <becarioit@farmapiel.com>
2024-07-26 09:23:08 -0600

    Getters Usuarios_table log

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,24 @@
 import DB.Usuarios_table;
+import Vistas.loginView;
 
 import javax.swing.*;
 
 
 class Main{
     public static void main(String[] args) {
-        UIManager.LookAndFeelInfo info[] = UIManager.getInstalledLookAndFeels();
+        /*UIManager.LookAndFeelInfo info[] = UIManager.getInstalledLookAndFeels();
         for(UIManager.LookAndFeelInfo look: info)
-            System.out.println(look.getClassName());
+            System.out.println(look.getClassName());*/
 
         Usuarios_table conexion = new Usuarios_table();
+
+        //UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
+        JFrame frame = new JFrame("Gestor de Tareas");
+        frame.setContentPane(new loginView().rootPanel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setSize(400,400);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
--- src/Vistas/loginView.java
+++ src/Vistas/loginView.java
@@ -1,11 +1,12 @@
 package Vistas;
 
 import javax.swing.*;
-import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
-public class loginView //extends JPanel
+public class loginView extends JPanel
     {
-    private JPanel rootPanel;
+    public JPanel rootPanel;
     private JLabel tituloLabel;
     private JTextField usuarioTextfield;
     private JButton entrarButton;
@@ -13,45 +14,16 @@ public class loginView //extends JPanel
     private JLabel passwordLabel;
     private JTextField passwordTextfield;
     private JLabel mensajeLabel;
+    private JLabel registrationLabel;
 
-//    @Override
-//    protected void paintComponent(Graphics g) {
-//        super.paintComponent(g);
-//        Graphics2D g2d = (Graphics2D) g;
-//        g2d.setRenderingHint(RenderingHints.KEY_RENDERING, RenderingHints.VALUE_RENDER_QUALITY);
-//        int w = getWidth(), h = getHeight();
-//        Color color1 = Color.RED;
-//        Color color2 = Color.GREEN;
-//        GradientPaint gp = new GradientPaint(0, 0, color1, w, h, color2);
-//        g2d.setPaint(gp);
-//        g2d.fillRect(0, 0, w, h);
-//    }
+        public loginView() {
+            registrationLabel.addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    super.mouseClicked(e);
 
-    public static void main(String[] args) throws Exception{
-        UIManager.setLookAndFeel("com.sun.java.swing.plaf.windows.WindowsClassicLookAndFeel");
-        JFrame frame = new JFrame("loginView");
-        frame.setContentPane(new loginView().rootPanel);
-        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
-        frame.setSize(400,400);
-        frame.setUndecorated(true);
-        frame.setOpacity(0);
-        frame.setVisible(true);
-    }
-
-//    public static void main(String[] args) {
-//        SwingUtilities.invokeLater(new Runnable() {
-//            @Override
-//            public void run() {
-//                JFrame frame = new JFrame();
-//                loginView panel = new loginView();
-//                frame.add(panel);
-//                frame.setSize(200, 200);
-//                frame.setLocationRelativeTo(null);
-//                frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//                frame.setVisible(true);
-//            }
-//        });
-//    }
+                }
+            });
+        }
 
 }
